2d08582ec4138babf580757197adfb1742be7fdd
HarmeetSingh25 <harmeetsingh6p@gmail.com>
2025-11-22 23:12:43 +0530

    Add Create note feature

--- Day-4/Server.js
+++ Day-4/Server.js
@@ -3,6 +3,14 @@ const app = express()
 
 app.use(express.json())
 
+let notes = []
+
+app.post("/notes", (req, res) => {
+    console.log(req.body);
+    notes.push(req.body)
+    res.json("Notes is added ")
+
+})
 
 
 app.listen(3000, () => {
