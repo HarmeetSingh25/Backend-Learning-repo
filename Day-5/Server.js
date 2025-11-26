8700bf37b22f90f39d069f76119cb129f59c14e7
HarmeetSingh25 <harmeetsingh6p@gmail.com>
2025-11-26 20:46:59 +0530

    Modify the code

--- Day-5/Server.js
+++ Day-5/Server.js
@@ -1,5 +1,10 @@
 const expres = require("express")
 const app = expres()
+
+app.post("/",(req,res)=>{
+res.json("Welcome to the home page ")
+})
+
 app.listen(3000, () => {
     console.log("Server is running ");
 
