cb10a97f9f841f3110018cb6e28643089f709d9c
HarmeetSingh25 <harmeetsingh6p@gmail.com>
2026-01-25 18:15:51 +0530

    Do changes in day 1

--- Day-2/Server.js
+++ Day-2/Server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+ PORT = 3000
 app.get("/", (req, res) => {
     res.send("hello world")
 })
@@ -7,6 +8,6 @@ app.get("/about", (req, res) => {
     res.send("Welcome to about page");
 
 })
-app.listen(3000, () => {
-    console.log("Express Server is running");
+app.listen(PORT, () => {
+    console.log(`Express Server is running ${PORT}`);
 })
